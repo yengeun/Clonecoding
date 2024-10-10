b98968d4ec9ea0b922721b3900a94fca32cc95f9
yengeun <7613ez@gmail.com>
2024-10-10 16:46:55 +0900

    ADD:main_file

--- apple/src/App.js
+++ apple/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Main1 from './components/Main1';
 import './assets/common.scss';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
         <>
             <Navbar />
             <Header />
+            <Main1 />
         </>
     );
 }
---
+++ apple/src/components/Main1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main1 = () => {
+  return (
+    <div>Main1</div>
+  )
+}
+
+export default Main1;
