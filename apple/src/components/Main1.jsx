1ca48aa2f229b921fc05c1bf5d76f106de5821f8
yengeun <7613ez@gmail.com>
2024-10-17 16:27:44 +0900

    Feat: main2완성

--- apple/src/components/Main1.jsx
+++ apple/src/components/Main1.jsx
@@ -93,14 +93,6 @@ const Main1 = () => {
             </div>
           </div>
           <div className="card">
-            <img src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/store-card-40-macbook-air-202402?wid=800&hei=1000&fmt=p-jpg&qlt=95&.v=1707259289595" alt="MacBook Air" />
-            <div className="card-info2">
-              <h3>MacBook Air</h3>
-              <p>어디서나 종횡무진할 수 있도록.</p>
-              <span>₩1,390,000</span>
-            </div>
-          </div>
-          <div className="card">
             <img src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/store-card-40-ipad-air-202405?wid=800&hei=1000&fmt=jpeg&qlt=90&.v=1713308272877" alt="iPad Air" />
             <div className="card-info2">
               <h3>iPad Air</h3>
@@ -124,8 +116,16 @@ const Main1 = () => {
               <span>₩1,499,000부터</span>
             </div>
           </div>
+          <div className="card">
+            <img src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/store-card-40-macbook-air-202402?wid=800&hei=1000&fmt=p-jpg&qlt=95&.v=1707259289595" alt="MacBook Air" />
+            <div className="card-info2">
+              <h3>MacBook Air</h3>
+              <p>어디서나 종횡무진할 수 있도록.</p>
+              <span>₩1,390,000</span>
+            </div>
+          </div>
         </div>
-        {scrollIndex < 11 && ( 
+        {scrollIndex < 14 && ( 
           <button className="arrowr" onClick={scrollRight}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
               <path d="M23.5587,16.916 C24.1447,17.4999987 24.1467,18.446 23.5647,19.034 L16.6077,26.056 C16.3147,26.352 15.9287,26.4999987 15.5427,26.4999987 C15.1607,26.4999987 14.7787,26.355 14.4867,26.065 C13.8977,25.482 13.8947,24.533 14.4777,23.944 L20.3818,17.984 L14.4408,12.062 C13.8548,11.478 13.8528,10.5279 14.4378,9.941 C15.0218,9.354 15.9738,9.353 16.5588,9.938 L23.5588,16.916 L23.5587,16.916 Z"></path>
