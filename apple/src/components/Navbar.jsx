1c1addead33a8a986f1989d3333c367e7329a1f2
yengeun <7613ez@gmail.com>
2024-11-04 15:24:47 +0900

    Feat:nav 수정중

--- apple/src/components/Navbar.jsx
+++ apple/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaApple } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import { IoBagOutline } from "react-icons/io5";
@@ -6,77 +6,195 @@ import '../assets/Navbar.scss';
 import '../assets/common.scss';
 
 const Navbar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsDropdownOpen(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsDropdownOpen(false);
-  };
-
-  return (
-    <header className="navbar">
-      <nav className="navbar-menu">
-        <ul>
-          <li>
-            <a href="#"><FaApple size={20} /></a>
-          </li>
-          <li 
-            className="has-dropdown"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <a href="#">스토어</a>
-            <div className={`dropdown-menu ${isDropdownOpen ? 'active' : ''}`}>
-              <div className="dropdown-column">
-                <h4>쇼핑하기</h4>
-                <ul>
-                  <li><a href="#">최신 제품 쇼핑하기</a></li>
-                  <li><a href="#">Mac</a></li>
-                  <li><a href="#">iPad</a></li>
-                  <li><a href="#">iPhone</a></li>
-                  <li><a href="#">Apple Watch</a></li>
-                  <li><a href="#">액세서리</a></li>
-                </ul>
-              </div>
-              <div className="dropdown-column">
-                <h4>빠른 링크</h4>
-                <ul>
-                  <li><a href="#">매장 찾기</a></li>
-                  <li><a href="#">주문 상태</a></li>
-                  <li><a href="#">Apple Trade In</a></li>
-                  <li><a href="#">할부 방식</a></li>
-                </ul>
-              </div>
-              <div className="dropdown-column">
-                <h4>특별 할인 쇼핑하기</h4>
-                <ul>
-                  <li><a href="#">인증 리퍼비쉬 제품</a></li>
-                  <li><a href="#">교육</a></li>
-                  <li><a href="#">비즈니스</a></li>
-                </ul>
-              </div>
+    return (
+        <header className="nav">
+            <div className="nav__inner">
+                <div className="nav__logo">
+                    <a href="#"><FaApple size={20} /></a>
+                </div>
+                <nav className="nav__menu">
+                    <ul>
+                        <li className="menu-item">
+                            <a href="#">스토어</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column1">
+                                        <h4>쇼핑하기</h4>
+                                        <ul>
+                                            <li>최신 제품 쇼핑하기</li>
+                                            <li>MAC</li>
+                                            <li>iPad</li>
+                                            <li>iPhone</li>
+                                            <li>Apple Watch</li>
+                                            <li>액세서리</li>
+                                        </ul>
+                                        <h4>빠른 링크</h4>
+                                        <ul>
+                                            <li>매장 찾기</li>
+                                            <li>주문 상태</li>
+                                            <li>Apple Trade In</li>
+                                            <li>할부 방식</li>
+                                        </ul>
+                                        <h4>특별 할인 쇼핑하기</h4>
+                                        <ul>
+                                            <li>인증 리퍼비쉬 제품</li>
+                                            <li>교육</li>
+                                            <li>비즈니스</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">MAC</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column2">
+                                        <h3>MAC</h3>
+                                        <ul>
+                                            <li>전체보기</li>
+                                            <li>백팩</li>
+                                            <li>숄더 / 토트백</li>
+                                            <li>메신저 / 크로스백</li>
+                                            <li>슬링백 / 허쉘</li>
+                                            <li>모자</li>
+                                            <li>양말</li>
+                                            <li>테니스</li>
+                                            <li>기타</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">iPad</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column3">
+                                        <h3>iPad</h3>
+                                        <ul>
+                                            <li>전체보기</li>
+                                            <li>패키지</li>
+                                            <li>와이어브라</li>
+                                            <li>노와이어브라</li>
+                                            <li>데일리브라탑</li>
+                                            <li>스포츠브라탑</li>
+                                            <li>팬티</li>
+                                            <li>사각드로즈</li>
+                                            <li>이지웨어</li>
+                                            <li>파자마</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">iPhone</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column4">
+                                        <h3>iPhone</h3>
+                                        <ul>
+                                            <li>테니스</li>
+                                            <li>러닝/트레이닝</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">Watch</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column5">
+                                        <h3>Watch</h3>
+                                        <ul>
+                                            <li>AirPods</li>
+                                            <li>러닝/트레이닝</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">AirPods</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column6">
+                                        <h3>AirPods</h3>
+                                        <ul>
+                                            <li>테니스</li>
+                                            <li>러닝/트레이닝</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">TV 및 홈</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column7">
+                                        <h3>TV 및 홈</h3>
+                                        <ul>
+                                            <li>테니스</li>
+                                            <li>러닝/트레이닝</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">엔터테인먼트</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column8">
+                                        <h3>엔터테인먼트</h3>
+                                        <ul>
+                                            <li>테니스</li>
+                                            <li>러닝/트레이닝</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">액세서리</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column9">
+                                        <h3>액세서리</h3>
+                                        <ul>
+                                            <li>테니스</li>
+                                            <li>러닝/트레이닝</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#">고객지원</a>
+                            <div className="dropdown">
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-column10">
+                                        <h3>고객지원</h3>
+                                        <ul>
+                                            <li>테니스</li>
+                                            <li>러닝/트레이닝</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="nav__icons">
+                    <a href="#"><IoIosSearch size={20} /></a>
+                    <a href="#"><IoBagOutline size={20} /></a>
+                </div>
             </div>
-          </li>
-          <li><a href="#">Mac</a></li>
-          <li><a href="#">iPad</a></li>
-          <li><a href="#">iPhone</a></li>
-          <li><a href="#">Watch</a></li>
-          <li><a href="#">AirPods</a></li>
-          <li><a href="#">TV 및 홈</a></li>
-          <li><a href="#">엔터테인먼트</a></li>
-          <li><a href="#">액세서리</a></li>
-          <li><a href="#">고객지원</a></li>
-          <li><a href="#"><IoIosSearch size={20} /></a></li>
-          <li><a href="#"><IoBagOutline size={20} /></a></li>
-        </ul>
-      </nav>
-      {/* 드롭다운 열렸을 때 배경 블러 처리 */}
-      <div className={`overlay ${isDropdownOpen ? 'blur' : ''}`} />
-    </header>
-  );
-};
+        </header>
+    );
+}
 
 export default Navbar;
